b9f3c95ddfb0c8316bd78ff5542fd845c5fff8f4
Abhishek Udupa <56753465+abhishekaudupa@users.noreply.github.com>
2023-12-20 18:16:17 +0530

    Update driver module 1. Updated the driver module to offer users choice of operations and execute the program accordingly.

--- driver.c
+++ driver.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 #include "argument.h"
+#include "create_index.h"
+#include "word_list_table.h"
 #include "allocator.h"
 
 int main(int argc, char **argv) {
 
-    for(int i = 0; i < argc; ++i)
-	printf("%s\n", argv[i]);
-
     //validate arguments and collect the validation array.
-    char *arg_val = validate_arguments(argc, argv);
+    char *arg_val_array = validate_arguments(argc, argv);
 
-    for(int i = 0; i < argc; ++i) {
-	if(arg_val[i])
-	    printf("Valid:   %s\n", argv[i]);
-	else
-	    printf("Invalid: %s\n", argv[i]);
+    //pointer to database index hash table.
+    Word_List_Table *index_table = NULL;
+
+    //superloop to ask for user choice.
+    while(1) {
+	printf("Select a choice:\n");
+	printf("1. Create Database\n");
+	printf("2. Display Database\n");
+	printf("3. Exit\n");
+	printf("choice: ");
+
+	int choice;
+	scanf("%d", &choice);
+
+	switch(choice) {
+	    case 1:
+		    //create the inverted index database.
+		    index_table = create_database(argc, argv, arg_val_array);
+		    break;
+	    case 2:
+		    if(index_table) {
+		    //display database
+		    print_table(index_table);
+		    } else {
+			fprintf(stderr, "Index not yet created.\n");
+		    }
+		    break;
+	    case 3:
+		    //exit program.
+		    free_all_memory();
+		    return 0;
+	    default:
+		fprintf(stderr, "Invalid choice.\n");
+		break;
+	}
     }
-
-    free_memory(arg_val);
-    free_all_memory();
 }
-
